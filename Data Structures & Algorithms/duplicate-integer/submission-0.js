9f7dab968bc6a0f028847e300764fc8ace138aac
Hamza Younes <63872228+hamzahyounes@users.noreply.github.com>
2026-09-24 23:12:30 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const seenSet = new Set();
+        for(const num of nums) {
+            if(seenSet.has(num)) return true;
+            seenSet.add(num);
+        }
+        return false;
+    }
+}
